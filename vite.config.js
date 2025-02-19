ce99de8d7a6a11da9764eba7880df8cd5fbdb53b
dersilly <37103938-dersilly@users.noreply.replit.com>
2025-02-19 08:15:42 +0000

    User checkpoint: Fix: Correct syntax error in `vite.config.js` and add security headers

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -7,6 +6,12 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     hmr: true,
-    headers: {}
-  }
+    headers: {
+      "Strict-Transport-Security": "max-age=86400; includeSubDomains", // Adds HSTS options to your website, with a expiry time of 1 day
+      "X-Content-Type-Options": "nosniff", // Protects from improper scripts runnings
+      "X-Frame-Options": "DENY", // Stops your site being used as an iframe
+      "X-XSS-Protection": "1; mode=block", // Gives XSS protection to legacy browsers
+      "content-type": "text/css; charset=utf-8",
+    },
+  },
 });
